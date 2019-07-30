efdabc5bfcf32a7b80008189c62ac4083119e97a
jauntywunderkind <jaunty+wunder+kind+git@voodoowarez.com>
2019-07-29 22:45:25 -0400

    Implement negative end indexes.

--- async-iter-slice.js
+++ async-iter-slice.js
@@ -1,17 +1,31 @@
 "use module"
-export async function* asyncAsyncIterRangeFilter( inputs, start= 0, end= -1){
+export async function* asyncAsyncIterRangeFilter( inputs, start= 0, end){
+	let buffer
+	let fill
 	if( end< 0){
-		if( end=== -1){
-			end= Number.POSITIVE_INFINITY
-		}else if( inputs.length=== undefined){
-			throw new Error("TODO: support negative 'end' for non-fixed length")
-		}else{
-			len= inputs.length+ end+ 1
-		}
+		buffer= new Array( end* -1)
+		fill= end
+		end= undefined
 	}
 
 	let i= 0
-	for await( const o of inputs){
+	for await( let o of inputs){
+		if( buffer){
+			let pos= fill
+			if( fill< 0){
+				// prebuffering
+				buffer[ buffer.length+ fill]= o
+				fill++
+				continue
+			}else{
+				// buffer filled, swap o& buffer
+				let stored= buffer[ fill]
+				buffer[ fill]= o
+				o= stored
+			}
+			fill= ( fill+ 1)% buffer.length
+		}
+
 		// emit this value
 		if( i>= start){
 			yield o
@@ -23,23 +37,45 @@ export async function* asyncAsyncIterRangeFilter( inputs, start= 0, end= -1){
 	}
 }
 
-export function* syncAsyncIterRangeFilter( inputs, start, end= -1){
+export function* syncAsyncIterRangeFilter( inputs, start, end){
+	let buffer
+	let fill
 	if( end< 0){
-		len= inputs.length+ end+ 1
+		buffer= new Array( end* -1)
+		fill= end
+		end= undefined
 	}
 
 	let i= 0
-	for( const o of inputs){
+	for( let o of inputs){
+		if( buffer){
+			let pos= fill
+			if( fill< 0){
+				// prebuffering
+				buffer[ buffer.length+ fill]= o
+				fill++
+				continue
+			}else{
+				// buffer filled, swap o& buffer
+				let stored= buffer[ fill]
+				buffer[ fill]= o
+				o= stored
+			}
+			fill= ( fill+ 1)% buffer.length
+		}
+
+		// emit this value
 		if( i>= start){
 			yield o
 		}
+		// advance & check end
 		if( ++i>= end){
 			return
 		}
 	}
 }
 
-export function asyncIterRangeFilter( inputs, start= 0, end= -1){
+export function asyncIterRangeFilter( inputs, start= 0, end){
 	if( inputs[ Symbol.asyncIterator]){
 		return asyncAsyncIterRangeFilter( inputs, start, end)
 	}else{
